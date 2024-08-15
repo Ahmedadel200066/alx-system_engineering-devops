211446fcca2faca07cbf09319e442305759af7e9
Ahmedadel200066 <medo.adel200@gmail.com>
2024-08-16 02:29:21 +0300

    chore: Update top_ten function to retrieve and print the top ten posts from a given subreddit on Reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""0-subs"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+This function retrieves the top ten posts from a given subreddit on Reddit.
+Args:
+    subreddit (str): The name of the subreddit to retrieve posts from.
+Returns:
+    None
+    Raises:
+    requests.exceptions.HTTPError: If there is an HTTP error during the request.
+    KeyError: If there is a key error while accessing the JSON response.
+    ValueError: If there is a value error while processing the JSON response.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Ensure the request was successful
+
+        data = response.json()
+        posts = data['data']['children']
+
+        for post in posts:
+            print(post['data']['title'])
+
+    except (requests.exceptions.HTTPError, KeyError, ValueError):
+        print(None)
