e7b80821c1de0d29d5095151f37f741aede45e4c
Ahmedadel200066 <medo.adel200@gmail.com>
2024-08-16 02:21:24 +0300

    chore: Add script to retrieve number of subscribers for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0-subs"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        int: The number of subscribers for the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data['data']['subscribers']
+    except (requests.exceptions.HTTPError, KeyError):
+        return 0
