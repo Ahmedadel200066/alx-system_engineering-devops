37a48a8732e5167adf3528d597852601e3a98cbb
Ahmedadel200066 <medo.adel200@gmail.com>
2024-08-22 15:14:49 +0300

    Fix request limit at nginx and restart Nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,11 @@
-# fix request limit at nginx
+
+# This Puppet manifest file contains code to fix the value of a parameter in the /etc/default/nginx file and restart Nginx.
+
+# The first `exec` resource block named 'fix--for-nginx' uses the `sed` command to replace the value '15' with '4096' in the /etc/default/nginx file.
+
+# The second `exec` resource block named 'nginx-restart' is used to restart Nginx by executing the 'nginx restart' command.
+
+# Note: Make sure to provide the correct file path and ensure that the necessary permissions are set for executing the commands.
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
